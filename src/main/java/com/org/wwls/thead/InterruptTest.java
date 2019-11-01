f55bd9cb323fa0863f743ff533f29b0846f8cd9a
xudongdong <xudongdong@jianlc.com>
2019-11-01 20:43:23 +0800

    初始化上传

---
+++ src/main/java/com/org/wwls/thead/InterruptTest.java
@@ -0,0 +1,36 @@
+/*
+ *company:jlc
+ *author:xudd
+ *date:2019/10/30:21:21
+ *desc:{}
+ **/
+
+
+package com.org.wwls.thead;
+
+/**
+ * @author:xudd
+ * @date:2019/10/30 -21:21
+ * @desc:
+ **/
+public class InterruptTest {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // 循环中检测当前线程的中断状态
+                for (int i = 0; i < Integer.MAX_VALUE && !Thread.currentThread().isInterrupted(); i++) {
+                    System.out.println(i);
+                }
+            }
+        });
+        thread.start();
+        Thread.sleep(100);
+        thread.interrupt();
+    }
+
+
+
+}
