e8b013c42c06bf8bf4f48b7ac6a62b7e78887f4b
xudongdong <xudongdong@jianlc.com>
2019-11-04 10:31:56 +0800

    提交新代码

---
+++ src/main/java/com/org/future/UserCall.java
@@ -0,0 +1,24 @@
+/*
+ *company:jlc
+ *author:xudd
+ *date:2019/11/1:21:13
+ *desc:{}
+ **/
+
+
+package com.org.future;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author:xudd
+ * @date:2019/11/1 -21:13
+ * @desc:
+ **/
+public class UserCall implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+
+        return "zs"+System.currentTimeMillis();
+    }
+}
