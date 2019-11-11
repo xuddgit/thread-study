a5d3a50e07037e37b59f236ed202e25449074309
xudongdong <1107582053@qq.com>
2019-11-11 09:05:20 +0800

    队列

---
+++ src/main/java/com/org/counter/Counter1.java
@@ -0,0 +1,11 @@
+package com.org.counter;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter1 {
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/org/producer/consumer/Producer.java
@@ -0,0 +1,63 @@
+package com.org.producer.consumer;
+
+import java.util.Queue;
+import java.util.Random;
+
+/**
+ * 生产者
+ */
+public class Producer extends Thread{
+
+    private Queue<Integer> queue;
+
+    String name;
+
+    int maxSize;
+
+    int i=0;
+
+    public Producer(String name,Queue<Integer> queue,int maxSize){
+        super(name);
+
+        this.name=name;
+        this.queue=queue;
+        this.maxSize=maxSize;
+
+    }
+
+    public void run(){
+        while(true){
+            synchronized (queue){
+               while (queue.size()==maxSize){
+                   try{
+                       System.out.println("Queue is full ,Producer["+name+"]thread waiting for "
+                               +"consumer to take someting from queue .");
+                                queue.wait();
+
+                   }catch (Exception ex){
+                       ex.printStackTrace();
+                   }
+
+               }
+               System.out.println("["+name +"] producing value: +"+i );
+               queue.offer(i++);
+               queue.notifyAll();
+
+               try{
+                   Thread.sleep(new Random().nextInt(1000));
+
+               }catch (InterruptedException e){
+                   e.printStackTrace();
+               }
+
+            }
+
+        }
+    }
+
+
+
+
+
+
+}
