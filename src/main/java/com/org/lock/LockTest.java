9c3e30000aa12cf6d160fd42a5fc9795356fd0a0
xudongdong <1107582053@qq.com>
2019-11-14 22:59:30 +0800

    读写锁和公平锁

---
+++ src/main/java/com/org/lock/LockTest.java
@@ -0,0 +1,54 @@
+package com.org.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class LockTest {
+
+    private static final  ReentrantLock lock = new ReentrantLock();
+
+    private final ReentrantReadWriteLock  readWriteLock = new ReentrantReadWriteLock ();
+    public static void main(String[] args) {
+
+        new Thread(()->{
+            lock.lock();
+
+            lock.lock();
+
+            try{
+                System.out.println("锁1执行完毕");
+            }catch (Exception e){
+                e.printStackTrace();
+
+            }finally {
+                lock.unlock();
+            }
+
+
+        }).start();
+
+        new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            lock.lock();
+
+            try{
+                System.out.println("锁2执行完毕");
+            }catch (Exception e){
+                e.printStackTrace();
+
+            }finally {
+                lock.unlock();
+            }
+
+
+        }).start();
+
+
+    }
+
+}
---
+++ src/main/java/com/org/lock/LockTest2.java
@@ -0,0 +1,53 @@
+package com.org.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class LockTest2 {
+
+
+    private static final ReentrantReadWriteLock  readWriteLock = new ReentrantReadWriteLock ();
+    public static void main(String[] args) {
+
+        new Thread(()->{
+            readWriteLock.readLock().lock();
+
+            readWriteLock.readLock().lock();
+
+            try{
+                System.out.println("锁1执行完毕");
+            }catch (Exception e){
+                e.printStackTrace();
+
+            }finally {
+                readWriteLock.readLock().unlock();
+            }
+
+
+        }).start();
+
+        new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            readWriteLock.readLock().lock();
+
+            try{
+                System.out.println("锁2执行完毕");
+            }catch (Exception e){
+                e.printStackTrace();
+
+            }finally {
+                readWriteLock.readLock().unlock();
+            }
+
+
+        }).start();
+
+
+    }
+
+}
