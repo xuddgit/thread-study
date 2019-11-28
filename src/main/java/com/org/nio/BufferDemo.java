766d7d7282a7b6ee39c1b6ed7a6af354d8e43075
xudongdong <xudongdong@jianlc.com>
2019-11-28 20:11:33 +0800

    NIO学习

---
+++ src/main/java/com/org/nio/BufferDemo.java
@@ -0,0 +1,101 @@
+/*
+ *company:jlc
+ *author:xudd
+ *date:2019/11/28:19:49
+ *desc:{}
+ **/
+
+
+package com.org.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+/**
+ * @author:xudd
+ * @date:2019/11/28 -19:49
+ * @desc:
+ **/
+public class BufferDemo {
+   private static IntBuffer intBuffer = IntBuffer.allocate(20);
+    public static void main(String[] args) {
+        allocatTest();
+        putTest();
+        flipTest();
+        getTest();
+        rewindtest();
+    }
+
+    public static void allocatTest()
+    {
+
+        System.out.println("------------after allocate------------------");
+        System.out.println("position=" + intBuffer.position());
+        System.out.println("limit=" + intBuffer.limit());
+        System.out.println("capacity=" + intBuffer.capacity());
+    }
+
+
+    public static void putTest() {
+        for (int i = 0; i < 5; i++) {
+            intBuffer.put(i);
+
+
+        }
+        System.out.println("------------after put------------------");
+        System.out.println("position=" + intBuffer.position());
+        System.out.println("limit=" + intBuffer.limit());
+        System.out.println("capacity=" + intBuffer.capacity());
+        /**flip()方法主要是从读模式切换成写模式，调用规则是：(1)首先设置可读的长度limit .将写模式下的Buffer中
+         * 内容的最后位置position值编为读模式下的limit位置值，新的limit值作为读越界位置；*/
+    }
+
+    public static void flipTest(){
+        intBuffer.flip();
+
+        System.out.println("------------after flip------------------");
+        System.out.println("position=" + intBuffer.position());
+        System.out.println("limit=" + intBuffer.limit());
+        System.out.println("capacity=" + intBuffer.capacity());
+
+    }
+
+
+    public static void getTest(){
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            int j = intBuffer.get();
+            System.out.println("j = " + j);
+        }
+
+        System.out.println("------------after get2------------------");
+        System.out.println("position=" + intBuffer.position());
+        System.out.println("limit=" + intBuffer.limit());
+        System.out.println("capacity=" + intBuffer.capacity());
+
+        for (int i = 0; i < 3; i++)
+        {
+            int j = intBuffer.get();
+            System.out.println("j = " + j);
+        }
+        System.out.println("------------after get3------------------");
+        System.out.println("position=" + intBuffer.position());
+        System.out.println("limit=" + intBuffer.limit());
+        System.out.println("capacity=" + intBuffer.capacity());
+
+    }
+
+
+    public static void rewindtest(){
+
+        intBuffer.rewind();
+        System.out.println("------------after rewind------------------");
+        System.out.println("position=" + intBuffer.position());
+        System.out.println("limit=" + intBuffer.limit());
+        System.out.println("capacity=" + intBuffer.capacity());
+
+    }
+
+}
---
+++ src/main/java/com/org/nio/nio.buffer.md
@@ -0,0 +1,22 @@
+Buffer类型
+
+ByteBuffer 类型
+CharBuffer 类型
+DoubleBuffer类型
+FloatBuffer 类型
+IntBuffer 类型
+LongBuffer类型
+ShortBuffer类型
+MappedByteBuffer类型
+
+**Buffer四个属性**
+
+capacity 容量
+
+limit 上界 ，缓冲区当前数量
+
+position 位置，下一个要读取或写的元素的索引
+
+mark (位置标记)
+
+
