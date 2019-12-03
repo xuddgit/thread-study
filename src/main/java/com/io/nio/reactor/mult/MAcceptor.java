b97e7a9df5bc19551398715f81e476a7b2e655ce
xudongdong <xudongdong@jianlc.com>
2019-12-03 19:07:52 +0800

    删出冗余代码

--- src/main/java/com/io/nio/reactor/mult/MAcceptor.java
+++ src/main/java/com/io/nio/reactor/mult/MAcceptor.java
@@ -1,7 +1,5 @@
 package com.io.nio.reactor.mult;
 
-import com.io.nio.reactor.single.ReactorHandler;
-
 import java.io.IOException;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
