5870c73aa0c31875667c1569ed4b8a71c1bae3f0
xudongdong <xudongdong@jianlc.com>
2019-12-11 17:51:26 +0800

    添加循环依赖测试

---
+++ src/test/java/com/org/wwls/Test4.java
@@ -0,0 +1,30 @@
+/*
+ *company:jlc
+ *author:xudd
+ *date:2019/12/5:19:52
+ *desc:{}
+ **/
+
+
+package com.org.wwls;
+
+import java.math.BigDecimal;
+
+/**
+ * @author:xudd
+ * @date:2019/12/5 -19:52
+ * @desc:
+ **/
+public class Test4 {
+
+    public static void main(String[] args) {
+        BigDecimal bigDecimal = BigDecimal.ZERO;
+
+        BigDecimal bigDecimal1= BigDecimal.ZERO;
+
+        bigDecimal=  bigDecimal.add(new BigDecimal(1));
+        System.out.println(bigDecimal);
+        System.out.println(bigDecimal1);
+
+    }
+}
