97ea5d42310b251dab2b7d22a60a6ef5e4c36b6f
xudongdong <xudongdong@jianlc.com>
2020-03-18 19:21:25 +0800

    代码初始化上传

---
+++ src/main/java/com/thread/local/ThreadLocalSimpleDemo.java
@@ -0,0 +1,23 @@
+/*
+ *company:jlc
+ *author:xudd
+ *date:2020/1/17:15:57
+ *desc:{}
+ **/
+
+
+package com.thread.local;
+
+/**
+ * @author:xudd
+ * @date:2020/1/17 -15:57
+ * @desc:
+ **/
+public class ThreadLocalSimpleDemo {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+
+        System.out.println("测试ThreadLocal");
+
+    }
+}
